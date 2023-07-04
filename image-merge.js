61e4e751956956c8b6f9fbb79c040d012855f601
itorr <itorrrrrr@me.com>
2023-07-04 20:41:56 +0800

    基础逻辑

--- image-merge.js
+++ image-merge.js
@@ -1,1 +1,44 @@
 const canvas = document.querySelector('canvas');
+
+
+
+
+let captureImage;
+let captureImageURL = '7eyih3xg.jpg';
+
+const loadImageByURL = (url,onLoad)=>{
+    const img = new Image();
+    img.onload = ()=>onLoad(img);;
+    img.crossOrigin = 'anonymous';
+    img.src = url;
+};
+const loadCaptureImageURL = url=>{
+    loadImageByURL(url,img=>{
+        captureImage = img;
+        drawMergeImage();
+    });
+};
+
+
+loadCaptureImageURL(captureImageURL);
+
+const height = 1080;
+
+const drawMergeImage = ()=>{
+    if(!captureImage){
+        alert('截图图片不能为空');
+        return;
+    }
+
+    // console.log(captureImage);
+    const { naturalWidth, naturalHeight } = captureImage;
+
+    const rate = naturalWidth / naturalHeight;
+
+    const outputWidth = height * rate;
+    const outputHeight = height * 2;
+
+    canvas.width = outputWidth;
+    canvas.height = outputHeight;
+
+}
